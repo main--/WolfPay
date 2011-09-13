d0045139603f1da114990d48c37275369fc3aff2
main() <main@main.dd-dns.de>
2011-09-13 17:04:07 +0200

    Added some code (eclipse-project)

---
+++ WolfPay/src/me/main__/WolfPay/WolfPay.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package me.main__.WolfPay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author main()
+ *
+ */
+public class WolfPay extends JavaPlugin {
+
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.Plugin#onDisable()
+	 */
+	public void onDisable() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.Plugin#onEnable()
+	 */
+	public void onEnable() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
