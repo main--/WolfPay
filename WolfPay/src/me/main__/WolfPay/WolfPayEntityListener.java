d6f0216beffe6310c70aa00855449bbda1f94082
main() <main@main.dd-dns.de>
2011-09-18 11:08:19 +0200

    Added more permission-nodes

--- WolfPay/src/me/main__/WolfPay/WolfPayEntityListener.java
+++ WolfPay/src/me/main__/WolfPay/WolfPayEntityListener.java
@@ -29,6 +29,11 @@ public class WolfPayEntityListener extends EntityListener {
 				//next: is he inside the free-limit?
 				List<Wolf> wolves = Util.getWolves(tamer);
 				wolves.add(wolf); //since this event isn't fully processed, we need to add the wolf manually.
+				if (Util.permission(tamer, "wolfpay.unlimited"))
+				{
+					//no message
+					wolf.setOwner(tamer);
+				}
 				int allowedwolves = WolfPay.freewolves;
 				if (WolfPay.savebought)
 				{
@@ -45,50 +50,47 @@ public class WolfPayEntityListener extends EntityListener {
 					tamer.sendMessage(ChatColor.GREEN.toString() + WolfPay.getMessage("havenow")
 							.replaceAll("x", String.valueOf(wolves.size()))
 							.replaceAll("y", String.valueOf(allowedwolves)));
-					if (wolves.size() == allowedwolves) //if he has reached the limit
-						tamer.sendMessage(ChatColor.YELLOW.toString() + WolfPay.getMessage("nextpay"));
+					
+					if (Util.permission(tamer, "wolfpay.pay")) //if he can bypass the limit (pay)
+						if (wolves.size() == allowedwolves) //if he has reached the limit
+							tamer.sendMessage(ChatColor.YELLOW.toString() + WolfPay.getMessage("nextpay"));
 					
 					wolf.setOwner(tamer);
 				}
 				else
 				{
-					//then: check if he has enough money
-					Holdings balance = iConomy.getAccount(tamer.getName()).getHoldings();
-					if (balance.hasEnough(WolfPay.price))
+					if (Util.permission(tamer, "wolfpay.pay"))
 					{
-						//yay, let's go on!
-						balance.subtract(WolfPay.price);
-						tamer.sendMessage(ChatColor.GREEN + WolfPay.getMessage("successpay")
-								.replaceAll("money", iConomy.format(WolfPay.price)));
-						
-						wolf.setOwner(tamer);
-						
-						if (WolfPay.savebought)
-						{
-							//we want to save this in the database
-							Account account = 
-									plugin.getDatabase().find(Account.class).where().ieq("playerName", tamer.getName()).findUnique();
-							
-							if (account == null)
-							{
-								account = new Account();
-								account.setBoughtwolves(1);
-								account.setPlayerName(tamer.getName());
+						//then: check if he has enough money
+						Holdings balance = iConomy.getAccount(tamer.getName()).getHoldings();
+						if (balance.hasEnough(WolfPay.price)) {
+							//yay, let's go on!
+							balance.subtract(WolfPay.price);
+							tamer.sendMessage(ChatColor.GREEN + WolfPay.getMessage("successpay")
+											.replaceAll("money", iConomy.format(WolfPay.price)));
+
+							wolf.setOwner(tamer);
+
+							if (WolfPay.savebought) {
+								//we want to save this in the database
+								Account account = plugin.getDatabase().find(Account.class).where().ieq("playerName", tamer.getName()).findUnique();
+
+								if (account == null) {
+									account = new Account();
+									account.setBoughtwolves(1);
+									account.setPlayerName(tamer.getName());
+								} else {
+									//upgrade the account
+									account.setBoughtwolves(account.getBoughtwolves() + 1);
+								}
+
+								plugin.getDatabase().save(account);
 							}
-							else
-							{
-								//upgrade the account
-								account.setBoughtwolves(account.getBoughtwolves() + 1);
-							}
-							
-							plugin.getDatabase().save(account);
+						} else {
+							tamer.sendMessage(ChatColor.RED.toString() + WolfPay.getMessage("notenoughmoney"));
+							event.setCancelled(true);
 						}
 					}
-					else
-					{
-						tamer.sendMessage(ChatColor.RED.toString() + WolfPay.getMessage("notenoughmoney"));
-						event.setCancelled(true);
-					}					
 				}
 			}
 			else
